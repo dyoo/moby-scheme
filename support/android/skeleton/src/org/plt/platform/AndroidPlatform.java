baeef40b33b66bfc46c0d82e833f20ddd8915413
Danny Yoo <dyoo@cs.wpi.edu>
2009-02-17 15:01:51 +0000

    Got the android platform communicating with the midlet.

--- support/android/skeleton/src/org/plt/platform/AndroidPlatform.java
+++ support/android/skeleton/src/org/plt/platform/AndroidPlatform.java
@@ -1,14 +1,18 @@
 package org.plt.platform;
 
-public class AndroidPlatform implements PlatformI {
-    // debug
-    static {
-	System.out.println("Android Platform");
-    }
 
+import android.app.Activity;
+
+
+public class AndroidPlatform implements PlatformI {
+    private Activity activity;
 
 
     public String getName() {
 	return "Android";
+    }
+
+    public void setActivity (Activity a) {
+	this.activity = a;
     }
 }
