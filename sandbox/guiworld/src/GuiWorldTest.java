1b02a86a89c67b653fb70cc4c5c9bb7be7bbc8ac
Zhe Zhang <zhe@cs.brown.edu>
2009-03-10 21:14:53 +0000

    Label transformer of button changed. Label becomes shorter

--- sandbox/guiworld/src/GuiWorldTest.java
+++ sandbox/guiworld/src/GuiWorldTest.java
@@ -4,55 +4,54 @@ import javax.microedition.midlet.*;
 import org.plt.guiworld.*;
 import org.plt.world.WorldTransformer;
 
-
 public class GuiWorldTest extends MIDlet {
-    private Object world;
+	private Object world;
 
-    public GuiWorldTest() {
-	this.world = new Integer(1);
-    }
+	public GuiWorldTest() {
+		this.world = new Integer(1);
+	}
 
-    public void startApp() throws MIDletStateChangeException {
-	Display display = Display.getDisplay(this);
+	public void startApp() throws MIDletStateChangeException {
+		Display display = Display.getDisplay(this);
 
-	Gui view = new Row(new Gui[] {
-	    new Message(new WorldTransformer() {
-		    public Object transform(Object world) {
-			return "Hello world";
-		    }
+		Gui view = new Row(new Gui[] { new Message(new WorldTransformer() {
+			public Object transform(Object world) {
+				return "Hello world";
+			}
 		}),
 
-
-	    new Message(new WorldTransformer() {
-		    // label
-		    public Object transform(Object world) {
-			return "The World says: " + world.toString();
-		    }
+		new Message(new WorldTransformer() {
+			// label
+			public Object transform(Object world) {
+				return "The World says: " + world.toString();
+			}
 		}),
 
-	    // Button has not yet been implemented.
-	    new Button(new WorldTransformer() {
-		    // label
-		    public Object transform(Object world) {
-			return "The World says: " + world.toString();
-		    }},
-		       
-		       // callback
-		       new WorldTransformer() {
-			   public Object transform(Object world) {
-			       return new Integer(((Integer)world).intValue() + 1);
-			   }})});
+		// Button has not yet been implemented.
+				new Button(new WorldTransformer() {
+					// label
+					public Object transform(Object world) {
+						return "says: " + world.toString();
+					}
+				},
 
+				// callback
+						new WorldTransformer() {
+							public Object transform(Object world) {
+								return new Integer(
+										((Integer) world).intValue() + 1);
+							}
+						}) });
 
-	GuiRenderer guiRender = new GuiRenderer("title", world, view);
-	Form form = guiRender.getForm();
-	display.setCurrent(form);
-    }
+		GuiRenderer guiRender = new GuiRenderer("title", world, view);
+		Form form = guiRender.getForm();
+		display.setCurrent(form);
+	}
 
-    public void pauseApp() {
-    }
+	public void pauseApp() {
+	}
 
-    public void destroyApp(boolean unconditional) {
-	notifyDestroyed();
-    }
+	public void destroyApp(boolean unconditional) {
+		notifyDestroyed();
+	}
 }
