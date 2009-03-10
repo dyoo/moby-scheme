aa8c6985b77fc22c316e5dc1e7ec1055a19d6888
Zhe Zhang <zhe@cs.brown.edu>
2009-03-10 23:57:06 +0000

    Added: constructor(), getValF()

--- support/common/src/org/plt/guiworld/TextField.java
+++ support/common/src/org/plt/guiworld/TextField.java
@@ -1,10 +1,20 @@
 package org.plt.guiworld;
 
+import org.plt.world.WorldTransformer;
+
 public class TextField implements Gui {
-    // Fill me in
 
+	private WorldTransformer valF;
 
-    public void accept(GuiVisitor v) {
-	v.visit(this);
-    }
+	public TextField(WorldTransformer valF) {
+		this.valF = valF;
+	}
+
+	public WorldTransformer getValF() {
+		return this.valF;
+	}
+
+	public void accept(GuiVisitor v) {
+		v.visit(this);
+	}
 }
