883161c576ea8d14604b4badebb4174eeab8d4b3
Zhe Zhang <zhe@cs.brown.edu>
2009-03-11 03:03:17 +0000

    Callback added

--- support/common/src/org/plt/guiworld/TextField.java
+++ support/common/src/org/plt/guiworld/TextField.java
@@ -1,17 +1,23 @@
 package org.plt.guiworld;
 
-import org.plt.world.WorldTransformer;
+import org.plt.world.*;
 
 public class TextField implements Gui {
 
 	private WorldTransformer valF;
+	private WorldAndObjectTransformer callback;
 
-	public TextField(WorldTransformer valF) {
+	public TextField(WorldTransformer valF, WorldAndObjectTransformer callback) {
 		this.valF = valF;
+		this.callback = callback;
 	}
 
 	public WorldTransformer getValF() {
 		return this.valF;
+	}
+
+	public WorldAndObjectTransformer getCallback() {
+		return callback;
 	}
 
 	public void accept(GuiVisitor v) {
