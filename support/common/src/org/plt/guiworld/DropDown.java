77d8726fa612a8a3a482162f19886297e01bcd95
Zhe Zhang <zhe@cs.brown.edu>
2009-03-11 18:50:35 +0000

    Added: callback

--- support/common/src/org/plt/guiworld/DropDown.java
+++ support/common/src/org/plt/guiworld/DropDown.java
@@ -4,13 +4,19 @@ import org.plt.world.WorldTransformer;
 
 public class DropDown implements Gui {
 	private WorldTransformer valF;
+	private WorldAndObjectTransformer callback;
 
-	public DropDown(WorldTransformer valF) {
+	public DropDown(WorldTransformer valF, WorldAndObjectTransformer callback) {
 		this.valF = valF;
+		this.callback = callback;
 	}
 
 	public WorldTransformer getValF() {
 		return this.valF;
+	}
+
+	public WorldAndObjectTransformer getCallback() {
+		return this.callback;
 	}
 
 	public void accept(GuiVisitor v) {
