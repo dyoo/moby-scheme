232210f3cce1694bd4c20e286a3b519b0354358e
Danny Yoo <dyoo@cs.wpi.edu>
2009-03-11 03:14:45 +0000

    guiworld android basic application.

---
+++ sandbox/guiworld-android/src/org/plt/GuiWorldTest.java
@@ -0,0 +1,15 @@
+package org.plt;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class GuiWorldTest extends Activity
+{
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
