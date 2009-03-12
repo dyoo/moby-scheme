4871b836231a2b831732d368c6ecb6f9297713b6
Zhe Zhang <zhe@cs.brown.edu>
2009-03-12 02:39:47 +0000

    Deleted: gui field in InitialGuiConstructor

--- support/android/skeleton/src/org/plt/guiworld/GuiRenderer.java
+++ support/android/skeleton/src/org/plt/guiworld/GuiRenderer.java
@@ -34,7 +34,7 @@ public class GuiRenderer {
 
 	// Initialize the form contents.
 	public void initializeForm() {
-		this.gui.accept(new InitialGuiConstructor(gui));
+		this.gui.accept(new InitialGuiConstructor());
 	}
 
 	// Changes the world, and updates the view accordingly.
@@ -44,14 +44,7 @@ public class GuiRenderer {
 	}
 
 	// ////////////////////////////////////////////////////////////////////
-
 	private class InitialGuiConstructor implements GuiVisitor {
-		private Gui gui;
-
-		public InitialGuiConstructor(Gui gui) {
-			this.gui = gui;
-		}
-
 		public void visit(Row r) {
 			Gui[] items = r.getItems();
 			for (int i = 0; i < items.length; i++) {
@@ -144,6 +137,106 @@ public class GuiRenderer {
 		public void visit(CheckBox c) {
 		}
 	}
+
+	// private class InitialGuiConstructor implements GuiVisitor {
+	// private Gui gui;
+	//
+	// public InitialGuiConstructor(Gui gui) {
+	// this.gui = gui;
+	// }
+	//
+	// public void visit(Row r) {
+	// Gui[] items = r.getItems();
+	// for (int i = 0; i < items.length; i++) {
+	// items[i].accept(this);
+	// }
+	// }
+	//
+	// public void visit(Message m) {
+	// String msg = (m.getValF().transform(world)).toString();
+	// TextView txt = new TextView(view.getContext());
+	// txt.setText(msg);
+	// view.addView(txt);
+	// }
+	//
+	// // FIXME: implement these!
+	//
+	// public void visit(final TextField t) {
+	// String txt = (t.getValF().transform(world)).toString();
+	// EditText edit = new EditText(view.getContext());
+	// edit.setText(txt);
+	// edit.addTextChangedListener(new TextWatcher() {
+	// public void afterTextChanged(Editable s) {
+	// String str = s.toString();
+	// Object newWorld = t.getCallback().transform(world, str);
+	// changeWorld(newWorld);
+	// }
+	//
+	// public void beforeTextChanged(CharSequence s, int start,
+	// int count, int after) {
+	//
+	// }
+	//
+	// public void onTextChanged(CharSequence s, int start, int count,
+	// int after) {
+	// }
+	// });
+	//
+	// view.addView(edit);
+	// }
+	//
+	// public void visit(Col c) {
+	// }
+	//
+	// public void visit(BoxGroup b) {
+	// }
+	//
+	// public void visit(Canvas c) {
+	// }
+	//
+	// public void visit(final org.plt.guiworld.Button b) {
+	// String label = (b.getValF().transform(world)).toString();
+	// android.widget.Button update = new android.widget.Button(view
+	// .getContext());
+	// update.setText(label);
+	// update.setOnClickListener(new OnClickListener() {
+	// public void onClick(View v) {
+	// Object newWorld = b.getCallback().transform(world);
+	// changeWorld(newWorld);
+	// }
+	// });
+	// view.addView(update);
+	// }
+	//
+	// public void visit(Slider s) {
+	// }
+	//
+	// public void visit(final DropDown d) {
+	// Object[] items = (Object[]) (d.getValF().transform(world));
+	// ArrayAdapter adapter = new ArrayAdapter(view.getContext(),
+	// android.R.layout.simple_list_item_1, items);
+	// adapter
+	// .setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+	// Spinner dropdown = new Spinner(view.getContext());
+	// dropdown.setAdapter(adapter);
+	// dropdown.setOnItemSelectedListener(new OnItemSelectedListener() {
+	// public void onItemSelected(AdapterView parent, View view,
+	// int position, long id) {
+	// Object item = parent.getSelectedItem();
+	// Object newWorld = d.getCallback().transform(world, item);
+	// changeWorld(newWorld);
+	// }
+	//
+	// public void onNothingSelected(AdapterView parent) {
+	//
+	// }
+	// });
+	// view.addView(dropdown);
+	// }
+	//
+	// public void visit(CheckBox c) {
+	// }
+	// }
 
 	private class GuiRefresher implements GuiVisitor {
 		private int viewIndex;
