c2e2a2a543404a9ab30085e07557d606374345fc
Zhe Zhang <zhe@cs.brown.edu>
2009-03-12 06:19:24 +0000

    Added: Object[] items now is a field of DropDown

--- support/common/src/org/plt/guiworld/DropDown.java
+++ support/common/src/org/plt/guiworld/DropDown.java
@@ -3,12 +3,19 @@ package org.plt.guiworld;
 import org.plt.world.*;
 
 public class DropDown implements Gui {
+	private Object[] items;
 	private WorldTransformer valF;
 	private WorldAndObjectTransformer callback;
 
-	public DropDown(WorldTransformer valF, WorldAndObjectTransformer callback) {
+	public DropDown(Object[] items, WorldTransformer valF,
+			WorldAndObjectTransformer callback) {
+		this.items = items;
 		this.valF = valF;
 		this.callback = callback;
+	}
+
+	public Object[] getItems() {
+		return this.items;
 	}
 
 	public WorldTransformer getValF() {
