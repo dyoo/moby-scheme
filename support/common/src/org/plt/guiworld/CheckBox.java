3925e37da8beda9856c9e4aa5cecf5d7b9a6295b
Zhe Zhang <zhe@cs.brown.edu>
2009-03-12 06:58:34 +0000

    Added: WorldTransformer valF as field

--- support/common/src/org/plt/guiworld/CheckBox.java
+++ support/common/src/org/plt/guiworld/CheckBox.java
@@ -1,10 +1,20 @@
 package org.plt.guiworld;
 
+import org.plt.world.WorldTransformer;
+
 public class CheckBox implements Gui {
-    // Fill me in
 
+	private WorldTransformer valF;
 
-    public void accept(GuiVisitor v) {
-	v.visit(this);
-    }
+	public CheckBox(WorldTransformer valF) {
+		this.valF = valF;
+	}
+
+	public WorldTransformer getValF() {
+		return this.valF;
+	}
+
+	public void accept(GuiVisitor v) {
+		v.visit(this);
+	}
 }
