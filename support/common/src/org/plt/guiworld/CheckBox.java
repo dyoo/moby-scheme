ed5f1ca54297d8aed76cb5bd6161ec5d5f47b5ef
Zhe Zhang <zhe@cs.brown.edu>
2009-03-12 07:04:35 +0000

    Added: WorldAndObjectTransformer callback as field

--- support/common/src/org/plt/guiworld/CheckBox.java
+++ support/common/src/org/plt/guiworld/CheckBox.java
@@ -5,6 +5,7 @@ import org.plt.world.WorldTransformer;
 public class CheckBox implements Gui {
 
 	private WorldTransformer valF;
+	private WorldAndObjectTransformer callback;
 
 	public CheckBox(WorldTransformer valF) {
 		this.valF = valF;
@@ -12,6 +13,10 @@ public class CheckBox implements Gui {
 
 	public WorldTransformer getValF() {
 		return this.valF;
+	}
+
+	public WorldAndObjectTransformer getCallback() {
+		return this.callback;
 	}
 
 	public void accept(GuiVisitor v) {
