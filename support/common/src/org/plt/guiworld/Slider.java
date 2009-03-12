1c3f45a30ad999860929b46d077ed41636adf221
Zhe Zhang <zhe@cs.brown.edu>
2009-03-12 05:26:52 +0000

    Added: min, max, cur, valF and callback as field

--- support/common/src/org/plt/guiworld/Slider.java
+++ support/common/src/org/plt/guiworld/Slider.java
@@ -1,10 +1,41 @@
 package org.plt.guiworld;
 
 public class Slider implements Gui {
-    // Fill me in
+	private Integer cur;
+	private Integer min;
+	private Integer max;
+	private WorldTransformer valF;
+	private WorldAndObjectTransformer callback;
 
+	public Slider(Integer min, Integer max, WorldTransformer valF,
+			WorldAndObjectTransformer callback) {
+		this.min = min;
+		this.max = max;
+		this.valF = valF;
+		this.callback = callback;
+	}
 
-    public void accept(GuiVisitor v) {
-	v.visit(this);
-    }
+	public Integer getMin() {
+		return this.min;
+	}
+
+	public Integer getMax() {
+		return this.max;
+	}
+
+	public Integer getCur() {
+		return this.cur;
+	}
+
+	public WorldTransformer getValF() {
+		return this.valF;
+	}
+
+	public WorldAndObjectTransformer getCallback() {
+		return this.callback;
+	}
+
+	public void accept(GuiVisitor v) {
+		v.visit(this);
+	}
 }
