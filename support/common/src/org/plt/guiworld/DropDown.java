f5be1a9fc673400e2d3eacc053692e310351c35b
Zhe Zhang <zhe@cs.brown.edu>
2009-03-13 18:17:26 +0000

    Add field: WorldTransformer valF

--- support/common/src/org/plt/guiworld/DropDown.java
+++ support/common/src/org/plt/guiworld/DropDown.java
@@ -3,13 +3,19 @@ package org.plt.guiworld;
 import org.plt.world.*;
 
 public class DropDown implements Gui {
+	private WorldTransformer valF;
 	private WorldTransformer choicesF;
 	private WorldAndObjectTransformer callback;
 
-	public DropDown(WorldTransformer choicesF,
+	public DropDown(WorldTransformer valF, WorldTransformer choicesF,
 			WorldAndObjectTransformer callback) {
+		this.valF = valF;
 		this.choicesF = choicesF;
 		this.callback = callback;
+	}
+
+	public WorldTransformer getValF() {
+		return this.valF;
 	}
 
 	public WorldTransformer getChoicesF() {
