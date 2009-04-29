c60d6945e5d66cc23b2a00a5c23bf84673d233a2
Danny Yoo <dyoo@cs.wpi.edu>
2009-04-29 15:01:22 -0400

    Turning Xinha into a source editor.

--- support/xinha/plugins/GetCode/GetCode.js
+++ support/xinha/plugins/GetCode/GetCode.js
@@ -29,10 +29,20 @@ GetCode._pluginInfo = {
     // Wrapper legacy see #442
     GetCode.prototype.getCode = function(editor)
     {
+	alert(editor._doc.body.innerHTML);
 	return _getCode(editor._doc.body, false, editor).join("");
     };
 
+
+    GetCode.prototype.toHtml = function(s) {
+	return htmlentities(s).replace('\n', "<br/>").replace(" ", "&nbsp;");
+    };
+
+
+
     var emptyAttributes = " checked disabled ismap readonly nowrap compact declare selected defer multiple noresize noshade "
+
+
 
     function _getCode(root, outputRoot, editor, indent)
     {
@@ -67,7 +77,7 @@ GetCode._pluginInfo = {
 	    break;
 
 	case 5: // Node.ENTITY_REFERENCE_NODE
-	    html.push('&' + root.nodeValue + ';');
+	    html.push(html_entity_decode('&' + root.nodeValue + ';'));
 	    break;
 
 	case 7: // Node.PROCESSING_INSTRUCTION_NODE
@@ -115,7 +125,8 @@ GetCode._pluginInfo = {
       else if ( outputRoot )
       {
         closed = (!(root.hasChildNodes() || Xinha.needsClosingTag(root)));
-        html.push(((Xinha.isBlockElement(root)) ? ('\n' + indent) : '') + "<" + root.tagName.toLowerCase());
+        html.push(((Xinha.isBlockElement(root)) ? ('\n' + indent) : ''));
+        // html.push(((Xinha.isBlockElement(root)) ? ('\n' + indent) : '') + "<" + root.tagName.toLowerCase());
         var attrs = root.attributes;
         
         for ( i = 0; i < attrs.length; ++i )
@@ -248,15 +259,15 @@ GetCode._pluginInfo = {
           if ( closed && root_tag=="p" )
           {
             //never use <p /> as empty paragraphs won't be visible
-            html.push(">&nbsp;</p>");
+            // html.push(">&nbsp;</p>");
           }
           else if ( closed )
           {
-            html.push(" />");
+            //html.push(" />");
           }
           else
           {
-            html.push(">");
+            //html.push(">");
           }
         }
       }
@@ -292,7 +303,7 @@ GetCode._pluginInfo = {
         }
         if ( outputRoot && !closed )
         {
-          html.push((((Xinha.isBlockElement(root) && containsBlock) || root_tag == 'head' || root_tag == 'html') ? ('\n' + indent) : '') + "</" + root.tagName.toLowerCase() + ">");
+//          html.push((((Xinha.isBlockElement(root) && containsBlock) || root_tag == 'head' || root_tag == 'html') ? ('\n' + indent) : '') + "</" + root.tagName.toLowerCase() + ">");
         }
       }
     break;
@@ -315,15 +326,262 @@ GetCode._pluginInfo = {
       }
       else
       {
-        html = [Xinha.htmlEncode(root.data)];
+        html = root.data; //[Xinha.htmlEncode(root.data)];
       }
     break;
 
     case 8: // Node.COMMENT_NODE
-      html = ["<!--" + root.data + "-->"];
+      // html = ["<!--" + root.data + "-->"];
     break;
   }
   return html;
+}
+
+
+
+
+
+
+
+
+
+// Grabbed from http://kevin.vanzonneveld.net/techblog/article/javascript_equivalent_for_phps_htmlentities/
+function htmlentities (string, quote_style) {
+    // http://kevin.vanzonneveld.net
+    // +   original by: Kevin van Zonneveld (http://kevin.vanzonneveld.net)
+    // +    revised by: Kevin van Zonneveld (http://kevin.vanzonneveld.net)
+    // +   improved by: nobbler
+    // +    tweaked by: Jack
+    // +   bugfixed by: Onno Marsman
+    // +    revised by: Kevin van Zonneveld (http://kevin.vanzonneveld.net)
+    // -    depends on: get_html_translation_table
+    // *     example 1: htmlentities('Kevin & van Zonneveld');
+    // *     returns 1: 'Kevin &amp; van Zonneveld'
+    // *     example 2: htmlentities("foo'bar","ENT_QUOTES");
+    // *     returns 2: 'foo&#039;bar'
+ 
+    var histogram = {}, symbol = '', tmp_str = '', entity = '';
+    tmp_str = string.toString();
+    
+    if (false === (histogram = get_html_translation_table('HTML_ENTITIES', quote_style))) {
+        return false;
+    }
+    
+    for (symbol in histogram) {
+        entity = histogram[symbol];
+        tmp_str = tmp_str.split(symbol).join(entity);
+    }
+    
+    return tmp_str;
+}
+
+
+// Grabbed from http://kevin.vanzonneveld.net/techblog/article/javascript_equivalent_for_phps_html_entity_decode/
+function html_entity_decode( string, quote_style ) {
+    // http://kevin.vanzonneveld.net
+    // +   original by: john (http://www.jd-tech.net)
+    // +      input by: ger
+    // +   improved by: Kevin van Zonneveld (http://kevin.vanzonneveld.net)
+    // +    revised by: Kevin van Zonneveld (http://kevin.vanzonneveld.net)
+    // +   bugfixed by: Onno Marsman
+    // +   improved by: marc andreu
+    // +    revised by: Kevin van Zonneveld (http://kevin.vanzonneveld.net)
+    // -    depends on: get_html_translation_table
+    // *     example 1: html_entity_decode('Kevin &amp; van Zonneveld');
+    // *     returns 1: 'Kevin & van Zonneveld'
+    // *     example 2: html_entity_decode('&amp;lt;');
+    // *     returns 2: '&lt;'
+ 
+    var histogram = {}, symbol = '', tmp_str = '', entity = '';
+    tmp_str = string.toString();
+    
+    if (false === (histogram = get_html_translation_table('HTML_ENTITIES', quote_style))) {
+        return false;
+    }
+ 
+    // &amp; must be the last character when decoding!
+    delete(histogram['&']);
+    histogram['&'] = '&amp;';
+ 
+    for (symbol in histogram) {
+        entity = histogram[symbol];
+        tmp_str = tmp_str.split(entity).join(symbol);
+    }
+    
+    return tmp_str;
+}
+
+
+// Grabbed from http://kevin.vanzonneveld.net/techblog/article/javascript_equivalent_for_phps_get_html_translation_table/
+
+function get_html_translation_table(table, quote_style) {
+    // http://kevin.vanzonneveld.net
+    // +   original by: Philip Peterson
+    // +    revised by: Kevin van Zonneveld (http://kevin.vanzonneveld.net)
+    // +   bugfixed by: noname
+    // +   bugfixed by: Alex
+    // +   bugfixed by: Marco
+    // +   bugfixed by: madipta
+    // %          note: It has been decided that we're not going to add global
+    // %          note: dependencies to php.js. Meaning the constants are not
+    // %          note: real constants, but strings instead. integers are also supported if someone
+    // %          note: chooses to create the constants themselves.
+    // %          note: Table from http://www.the-art-of-web.com/html/character-codes/
+    // *     example 1: get_html_translation_table('HTML_SPECIALCHARS');
+    // *     returns 1: {'"': '&quot;', '&': '&amp;', '<': '&lt;', '>': '&gt;'}
+    
+    var entities = {}, histogram = {}, decimal = 0, symbol = '';
+    var constMappingTable = {}, constMappingQuoteStyle = {};
+    var useTable = {}, useQuoteStyle = {};
+    
+    useTable      = (table ? table.toUpperCase() : 'HTML_SPECIALCHARS');
+    useQuoteStyle = (quote_style ? quote_style.toUpperCase() : 'ENT_COMPAT');
+    
+    // Translate arguments
+    constMappingTable[0]      = 'HTML_SPECIALCHARS';
+    constMappingTable[1]      = 'HTML_ENTITIES';
+    constMappingQuoteStyle[0] = 'ENT_NOQUOTES';
+    constMappingQuoteStyle[2] = 'ENT_COMPAT';
+    constMappingQuoteStyle[3] = 'ENT_QUOTES';
+    
+    // Map numbers to strings for compatibilty with PHP constants
+    if (!isNaN(useTable)) {
+        useTable = constMappingTable[useTable];
+    }
+    if (!isNaN(useQuoteStyle)) {
+        useQuoteStyle = constMappingQuoteStyle[useQuoteStyle];
+    }
+ 
+    if (useTable == 'HTML_SPECIALCHARS') {
+        // ascii decimals for better compatibility
+        entities['38'] = '&amp;';
+        if (useQuoteStyle != 'ENT_NOQUOTES') {
+            entities['34'] = '&quot;';
+        }
+        if (useQuoteStyle == 'ENT_QUOTES') {
+            entities['39'] = '&#039;';
+        }
+        entities['60'] = '&lt;';
+        entities['62'] = '&gt;';
+    } else if (useTable == 'HTML_ENTITIES') {
+        // ascii decimals for better compatibility
+      entities['38']  = '&amp;';
+        if (useQuoteStyle != 'ENT_NOQUOTES') {
+            entities['34'] = '&quot;';
+        }
+        if (useQuoteStyle == 'ENT_QUOTES') {
+            entities['39'] = '&#039;';
+        }
+      entities['60']  = '&lt;';
+      entities['62']  = '&gt;';
+      entities['160'] = '&nbsp;';
+      entities['161'] = '&iexcl;';
+      entities['162'] = '&cent;';
+      entities['163'] = '&pound;';
+      entities['164'] = '&curren;';
+      entities['165'] = '&yen;';
+      entities['166'] = '&brvbar;';
+      entities['167'] = '&sect;';
+      entities['168'] = '&uml;';
+      entities['169'] = '&copy;';
+      entities['170'] = '&ordf;';
+      entities['171'] = '&laquo;';
+      entities['172'] = '&not;';
+      entities['173'] = '&shy;';
+      entities['174'] = '&reg;';
+      entities['175'] = '&macr;';
+      entities['176'] = '&deg;';
+      entities['177'] = '&plusmn;';
+      entities['178'] = '&sup2;';
+      entities['179'] = '&sup3;';
+      entities['180'] = '&acute;';
+      entities['181'] = '&micro;';
+      entities['182'] = '&para;';
+      entities['183'] = '&middot;';
+      entities['184'] = '&cedil;';
+      entities['185'] = '&sup1;';
+      entities['186'] = '&ordm;';
+      entities['187'] = '&raquo;';
+      entities['188'] = '&frac14;';
+      entities['189'] = '&frac12;';
+      entities['190'] = '&frac34;';
+      entities['191'] = '&iquest;';
+      entities['192'] = '&Agrave;';
+      entities['193'] = '&Aacute;';
+      entities['194'] = '&Acirc;';
+      entities['195'] = '&Atilde;';
+      entities['196'] = '&Auml;';
+      entities['197'] = '&Aring;';
+      entities['198'] = '&AElig;';
+      entities['199'] = '&Ccedil;';
+      entities['200'] = '&Egrave;';
+      entities['201'] = '&Eacute;';
+      entities['202'] = '&Ecirc;';
+      entities['203'] = '&Euml;';
+      entities['204'] = '&Igrave;';
+      entities['205'] = '&Iacute;';
+      entities['206'] = '&Icirc;';
+      entities['207'] = '&Iuml;';
+      entities['208'] = '&ETH;';
+      entities['209'] = '&Ntilde;';
+      entities['210'] = '&Ograve;';
+      entities['211'] = '&Oacute;';
+      entities['212'] = '&Ocirc;';
+      entities['213'] = '&Otilde;';
+      entities['214'] = '&Ouml;';
+      entities['215'] = '&times;';
+      entities['216'] = '&Oslash;';
+      entities['217'] = '&Ugrave;';
+      entities['218'] = '&Uacute;';
+      entities['219'] = '&Ucirc;';
+      entities['220'] = '&Uuml;';
+      entities['221'] = '&Yacute;';
+      entities['222'] = '&THORN;';
+      entities['223'] = '&szlig;';
+      entities['224'] = '&agrave;';
+      entities['225'] = '&aacute;';
+      entities['226'] = '&acirc;';
+      entities['227'] = '&atilde;';
+      entities['228'] = '&auml;';
+      entities['229'] = '&aring;';
+      entities['230'] = '&aelig;';
+      entities['231'] = '&ccedil;';
+      entities['232'] = '&egrave;';
+      entities['233'] = '&eacute;';
+      entities['234'] = '&ecirc;';
+      entities['235'] = '&euml;';
+      entities['236'] = '&igrave;';
+      entities['237'] = '&iacute;';
+      entities['238'] = '&icirc;';
+      entities['239'] = '&iuml;';
+      entities['240'] = '&eth;';
+      entities['241'] = '&ntilde;';
+      entities['242'] = '&ograve;';
+      entities['243'] = '&oacute;';
+      entities['244'] = '&ocirc;';
+      entities['245'] = '&otilde;';
+      entities['246'] = '&ouml;';
+      entities['247'] = '&divide;';
+      entities['248'] = '&oslash;';
+      entities['249'] = '&ugrave;';
+      entities['250'] = '&uacute;';
+      entities['251'] = '&ucirc;';
+      entities['252'] = '&uuml;';
+      entities['253'] = '&yacute;';
+      entities['254'] = '&thorn;';
+      entities['255'] = '&yuml;';
+    } else {
+        throw Error("Table: "+useTable+' not supported');
+        return false;
+    }
+    
+    // ascii decimals to real symbols
+    for (decimal in entities) {
+        symbol = String.fromCharCode(decimal);
+        histogram[symbol] = entities[decimal];
+    }
+    
+    return histogram;
 }
 
 
