45242babc23d4b76e5e9c906c7371b69f68b1e04
Danny Yoo <dyoo@cs.wpi.edu>
2009-05-24 00:22:43 -0400

    committing in the really simple evaluator.

---
+++ support/js/read.js
@@ -0,0 +1,21 @@
+var readSchemeExpressions;
+
+(function(){
+
+
+//     function tokenize(s) {
+// 	var tokens = [];
+// 	while (s != "") {
+// 	}
+//     }
+
+
+
+    readSchemeExpressions = function(s) {
+	org.plt.Kernel.cons("42",
+			    org.plt.types.Empty.EMPTY);
+
+
+    
+
+    }}());
