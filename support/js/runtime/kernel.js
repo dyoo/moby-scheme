6d793e6ea91d450996cba54b5281a0a832931d96
Danny Yoo <dyoo@cs.wpi.edu>
2009-08-24 00:23:39 -0400

    documenting bugs, exposing plt.Kernel.MobySyntaxError.

--- support/js/runtime/kernel.js
+++ support/js/runtime/kernel.js
@@ -2063,6 +2063,7 @@ var plt = plt || {};
     // Expose the error classes.
     plt.Kernel.MobyError = MobyError;
     plt.Kernel.MobyTypeError = MobyTypeError;
+    plt.Kernel.MobySyntaxError = MobySyntaxError;
     plt.Kernel.MobyRuntimeError = MobyRuntimeError;
     
 })();
