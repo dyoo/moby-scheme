ba4668ff40134258d637cc7bb6daad4df3e8f906
Danny Yoo <dyoo@cs.wpi.edu>
2009-08-31 14:55:31 -0400

    Reindentation.

--- support/js/test/test-read.js
+++ support/js/test/test-read.js
@@ -62,73 +62,73 @@ function init() {
 
 
     return new Test.Unit.Runner({
-	    setup: function() {},
-    
-		teardown: function() {},
-    
-		testTokenizeSymbol: function() {
-		this.assert(isEqual(tokenize("hello"),
-				    [[["symbol", "hello"]], ""]));
-	    },
+	setup: function() {},
+	
+	teardown: function() {},
+	
+	testTokenizeSymbol: function() {
+	    this.assert(isEqual(tokenize("hello"),
+				[[["symbol", "hello"]], ""]));
+	},
 
-		testTokenizeSimpleList: function() {
-		this.assert(isEqual(tokenize("(hello world)"),
-				    [[["(", "("],
-				      ["symbol", "hello"],
-				      ["symbol", "world"],
-				      [")", ")"]], ""]));
-	    },
+	testTokenizeSimpleList: function() {
+	    this.assert(isEqual(tokenize("(hello world)"),
+				[[["(", "("],
+				  ["symbol", "hello"],
+				  ["symbol", "world"],
+				  [")", ")"]], ""]));
+	},
 
-		testTokenizeSimpleListAndString: function() {
-		this.assert(isEqual(tokenize("(hello \"world\")"),
-				    [[["(", "("],
-				      ["symbol", "hello"],
-				      ["string", "world"],
-				      [")", ")"]], ""]));
-	    },
-   
-		testReadSymbol: function() {
-		this.assert(schemeIsEqual(read("hello"),
-					  cons(symbol("hello"),
-					       empty)));
-	    },
+	testTokenizeSimpleListAndString: function() {
+	    this.assert(isEqual(tokenize("(hello \"world\")"),
+				[[["(", "("],
+				  ["symbol", "hello"],
+				  ["string", "world"],
+				  [")", ")"]], ""]));
+	},
+	
+	testReadSymbol: function() {
+	    this.assert(schemeIsEqual(read("hello"),
+				      cons(symbol("hello"),
+					   empty)));
+	},
 
 	testReadRational: function() {
 	    this.assert(schemeIsEqual(read("1"),
 				      cons(number(1), empty)));
-				      
+	    
 	},
 
 
 	testReadFloat: function() {
 	    this.assert(schemeIsEqual(read("0.1"),
 				      cons(makeFloat(0.1), empty)));
-				      
+	    
 	},
 
-		testReadSymbolInList: function() {
-		this.assert(schemeIsEqual(read("(hello again)"),
-					  cons(cons(symbol("hello"), cons(symbol("again"), empty)),
-					       empty)));
-	    },
+	testReadSymbolInList: function() {
+	    this.assert(schemeIsEqual(read("(hello again)"),
+				      cons(cons(symbol("hello"), cons(symbol("again"), empty)),
+					   empty)));
+	},
 
 
-		testReadListOfNumbers: function() {
-		this.assert(schemeIsEqual(read("(1) (2 3)"),
-					  cons(cons(number(1), empty),
-					       cons(cons(number(2), cons(number(3), empty)),
-						    empty))));
+	testReadListOfNumbers: function() {
+	    this.assert(schemeIsEqual(read("(1) (2 3)"),
+				      cons(cons(number(1), empty),
+					   cons(cons(number(2), cons(number(3), empty)),
+						empty))));
 
-		this.assert(schemeIsEqual(read("(1.4) (2 3)"),
-					  cons(cons(number(1.4), empty),
-					       cons(cons(number(2), cons(number(3), empty)),
-						    empty))));
+	    this.assert(schemeIsEqual(read("(1.4) (2 3)"),
+				      cons(cons(number(1.4), empty),
+					   cons(cons(number(2), cons(number(3), empty)),
+						empty))));
 
-		this.assert(schemeIsEqual(read("(1.4) (2 3)"),
-					  cons(cons(number(1.4), empty),
-					       cons(cons(number(2), cons(number(3), empty)),
-						    empty))));
-	    }
+	    this.assert(schemeIsEqual(read("(1.4) (2 3)"),
+				      cons(cons(number(1.4), empty),
+					   cons(cons(number(2), cons(number(3), empty)),
+						empty))));
+	}
 
     }); 
 }
