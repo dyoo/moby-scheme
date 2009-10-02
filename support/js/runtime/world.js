3e5189aef863a3931857cc291547b922d411a964
Danny Yoo <dyoo@cs.wpi.edu>
2009-10-01 23:42:44 -0400

    Added the color database; styles should be able to be strings now.

--- support/js/runtime/world.js
+++ support/js/runtime/world.js
@@ -258,6 +258,9 @@ plt.world.Kernel = plt.world.Kernel || {};
 
     // text: string number color -> TextImage
     plt.world.Kernel.text = function(aString, aSize, aColor) {
+	if (colorDb.get(aColor)) {
+	    aColor = colorDb.get(aColor);
+	}
 	return new TextImage
 	(aString, 
 	 plt.types.NumberTower.toInteger(aSize), 
@@ -267,6 +270,9 @@ plt.world.Kernel = plt.world.Kernel || {};
 
     // circle: number style color -> TextImage
     plt.world.Kernel.circle = function(aRadius, aStyle, aColor) {
+	if (colorDb.get(aColor)) {
+	    aColor = colorDb.get(aColor);
+	}
 	return new CircleImage
 	(plt.types.NumberTower.toInteger(aRadius), 
 	 aStyle,
@@ -280,6 +286,9 @@ plt.world.Kernel = plt.world.Kernel || {};
 
 
     plt.world.Kernel.nwRectangle = function(w, h, s, c) {
+	if (colorDb.get(c)) {
+	    c = colorDb.get(c);
+	}
 	var aRect = new RectangleImage
 	(plt.types.NumberTower.toInteger(w),
 	 plt.types.NumberTower.toInteger(h),
@@ -289,6 +298,9 @@ plt.world.Kernel = plt.world.Kernel || {};
     };
 
     plt.world.Kernel.rectangle = function(w, h, s, c) {
+	if (colorDb.get(c)) {
+	    c = colorDb.get(c);
+	}
 	// Fixme: get the pinholes!
 	return new RectangleImage(
 	    plt.types.NumberTower.toInteger(w),
@@ -419,8 +431,8 @@ plt.world.Kernel = plt.world.Kernel || {};
 
 
     RectangleImage.prototype.render = function(ctx, x, y) {
-	ctx.fillStyle = this.color;
-	if (this.style.toLowerCase() == "outline") {
+	ctx.fillStyle = this.color.toString();
+	if (this.style.toString().toLowerCase() == "outline") {
 	    ctx.strokeRect(x, y, this.width, this.height);
 	} else {
 	    ctx.fillRect(x, y, this.width, this.height);
@@ -487,11 +499,11 @@ plt.world.Kernel = plt.world.Kernel || {};
     CircleImage.prototype.render = function(ctx, x, y) {
 	ctx.translate(0, 0);
 	ctx.beginPath();
-	ctx.fillStyle = this.color;
+	ctx.fillStyle = this.color.toString();
 	ctx.arc(x + this.radius,
 		y + this.radius, 
 		this.radius, 0, 2*Math.PI, false);
-	if (this.style.toLowerCase() == "outline")
+	if (this.style.toString().toLowerCase() == "outline")
 	    ctx.stroke();
 	else
 	    ctx.fill();
@@ -626,7 +638,227 @@ plt.world.Kernel = plt.world.Kernel || {};
     
 
 //////////////////////////////////////////////////////////////////////////
- 
+
+
+
+
+
+
+
+
+
+    // Color database
+    function ColorDb() {
+	this.colors = {};
+    }
+    ColorDb.prototype.put = function(name, color) {
+	this.colors[name] = color;
+    };
+
+    ColorDb.prototype.get = function(name) {
+	return this.colors[name.toString().toUpperCase()];
+    };
+
+    function ColorRecord(r, g, b) {
+	this.r = r;
+	this.g = g;
+	this.b = b;
+    };
+
+    ColorRecord.prototype.toString = function() {
+	return "rgb(" + this.r + "," + this.g + "," + this.b + ")";
+    };
+
+    var colorDb = new ColorDb();
+    colorDb.put("ORANGE", new ColorRecord(255, 165, 0));
+    colorDb.put("RED", new ColorRecord(255, 0, 0));
+    colorDb.put("ORANGERED", new ColorRecord(255, 69, 0));
+    colorDb.put("TOMATO", new ColorRecord(255, 99, 71));
+    colorDb.put("DARKRED", new ColorRecord(139, 0, 0));
+    colorDb.put("RED", new ColorRecord(255, 0, 0));
+    colorDb.put("FIREBRICK", new ColorRecord(178, 34, 34));
+    colorDb.put("CRIMSON", new ColorRecord(220, 20, 60));
+    colorDb.put("DEEPPINK", new ColorRecord(255, 20, 147));
+    colorDb.put("MAROON", new ColorRecord(176, 48, 96));
+    colorDb.put("INDIAN RED", new ColorRecord(205, 92, 92));
+    colorDb.put("INDIANRED", new ColorRecord(205, 92, 92));
+    colorDb.put("MEDIUM VIOLET RED", new ColorRecord(199, 21, 133));
+    colorDb.put("MEDIUMVIOLETRED", new ColorRecord(199, 21, 133));
+    colorDb.put("VIOLET RED", new ColorRecord(208, 32, 144));
+    colorDb.put("VIOLETRED", new ColorRecord(208, 32, 144));
+    colorDb.put("LIGHTCORAL", new ColorRecord(240, 128, 128));
+    colorDb.put("HOTPINK", new ColorRecord(255, 105, 180));
+    colorDb.put("PALEVIOLETRED", new ColorRecord(219, 112, 147));
+    colorDb.put("LIGHTPINK", new ColorRecord(255, 182, 193));
+    colorDb.put("ROSYBROWN", new ColorRecord(188, 143, 143));
+    colorDb.put("PINK", new ColorRecord(255, 192, 203));
+    colorDb.put("ORCHID", new ColorRecord(218, 112, 214));
+    colorDb.put("LAVENDERBLUSH", new ColorRecord(255, 240, 245));
+    colorDb.put("SNOW", new ColorRecord(255, 250, 250));
+    colorDb.put("CHOCOLATE", new ColorRecord(210, 105, 30));
+    colorDb.put("SADDLEBROWN", new ColorRecord(139, 69, 19));
+    colorDb.put("BROWN", new ColorRecord(132, 60, 36));
+    colorDb.put("DARKORANGE", new ColorRecord(255, 140, 0));
+    colorDb.put("CORAL", new ColorRecord(255, 127, 80));
+    colorDb.put("SIENNA", new ColorRecord(160, 82, 45));
+    colorDb.put("ORANGE", new ColorRecord(255, 165, 0));
+    colorDb.put("SALMON", new ColorRecord(250, 128, 114));
+    colorDb.put("PERU", new ColorRecord(205, 133, 63));
+    colorDb.put("DARKGOLDENROD", new ColorRecord(184, 134, 11));
+    colorDb.put("GOLDENROD", new ColorRecord(218, 165, 32));
+    colorDb.put("SANDYBROWN", new ColorRecord(244, 164, 96));
+    colorDb.put("LIGHTSALMON", new ColorRecord(255, 160, 122));
+    colorDb.put("DARKSALMON", new ColorRecord(233, 150, 122));
+    colorDb.put("GOLD", new ColorRecord(255, 215, 0));
+    colorDb.put("YELLOW", new ColorRecord(255, 255, 0));
+    colorDb.put("OLIVE", new ColorRecord(128, 128, 0));
+    colorDb.put("BURLYWOOD", new ColorRecord(222, 184, 135));
+    colorDb.put("TAN", new ColorRecord(210, 180, 140));
+    colorDb.put("NAVAJOWHITE", new ColorRecord(255, 222, 173));
+    colorDb.put("PEACHPUFF", new ColorRecord(255, 218, 185));
+    colorDb.put("KHAKI", new ColorRecord(240, 230, 140));
+    colorDb.put("DARKKHAKI", new ColorRecord(189, 183, 107));
+    colorDb.put("MOCCASIN", new ColorRecord(255, 228, 181));
+    colorDb.put("WHEAT", new ColorRecord(245, 222, 179));
+    colorDb.put("BISQUE", new ColorRecord(255, 228, 196));
+    colorDb.put("PALEGOLDENROD", new ColorRecord(238, 232, 170));
+    colorDb.put("BLANCHEDALMOND", new ColorRecord(255, 235, 205));
+    colorDb.put("MEDIUM GOLDENROD", new ColorRecord(234, 234, 173));
+    colorDb.put("MEDIUMGOLDENROD", new ColorRecord(234, 234, 173));
+    colorDb.put("PAPAYAWHIP", new ColorRecord(255, 239, 213));
+    colorDb.put("MISTYROSE", new ColorRecord(255, 228, 225));
+    colorDb.put("LEMONCHIFFON", new ColorRecord(255, 250, 205));
+    colorDb.put("ANTIQUEWHITE", new ColorRecord(250, 235, 215));
+    colorDb.put("CORNSILK", new ColorRecord(255, 248, 220));
+    colorDb.put("LIGHTGOLDENRODYELLOW", new ColorRecord(250, 250, 210));
+    colorDb.put("OLDLACE", new ColorRecord(253, 245, 230));
+    colorDb.put("LINEN", new ColorRecord(250, 240, 230));
+    colorDb.put("LIGHTYELLOW", new ColorRecord(255, 255, 224));
+    colorDb.put("SEASHELL", new ColorRecord(255, 245, 238));
+    colorDb.put("BEIGE", new ColorRecord(245, 245, 220));
+    colorDb.put("FLORALWHITE", new ColorRecord(255, 250, 240));
+    colorDb.put("IVORY", new ColorRecord(255, 255, 240));
+    colorDb.put("GREEN", new ColorRecord(0, 255, 0));
+    colorDb.put("LAWNGREEN", new ColorRecord(124, 252, 0));
+    colorDb.put("CHARTREUSE", new ColorRecord(127, 255, 0));
+    colorDb.put("GREEN YELLOW", new ColorRecord(173, 255, 47));
+    colorDb.put("GREENYELLOW", new ColorRecord(173, 255, 47));
+    colorDb.put("YELLOW GREEN", new ColorRecord(154, 205, 50));
+    colorDb.put("YELLOWGREEN", new ColorRecord(154, 205, 50));
+    colorDb.put("MEDIUM FOREST GREEN", new ColorRecord(107, 142, 35));
+    colorDb.put("OLIVEDRAB", new ColorRecord(107, 142, 35));
+    colorDb.put("MEDIUMFORESTGREEN", new ColorRecord(107, 142, 35));
+    colorDb.put("DARK OLIVE GREEN", new ColorRecord(85, 107, 47));
+    colorDb.put("DARKOLIVEGREEN", new ColorRecord(85, 107, 47));
+    colorDb.put("DARKSEAGREEN", new ColorRecord(143, 188, 139));
+    colorDb.put("LIME", new ColorRecord(0, 255, 0));
+    colorDb.put("DARK GREEN", new ColorRecord(0, 100, 0));
+    colorDb.put("DARKGREEN", new ColorRecord(0, 100, 0));
+    colorDb.put("LIME GREEN", new ColorRecord(50, 205, 50));
+    colorDb.put("LIMEGREEN", new ColorRecord(50, 205, 50));
+    colorDb.put("FOREST GREEN", new ColorRecord(34, 139, 34));
+    colorDb.put("FORESTGREEN", new ColorRecord(34, 139, 34));
+    colorDb.put("SPRING GREEN", new ColorRecord(0, 255, 127));
+    colorDb.put("SPRINGGREEN", new ColorRecord(0, 255, 127));
+    colorDb.put("MEDIUM SPRING GREEN", new ColorRecord(0, 250, 154));
+    colorDb.put("MEDIUMSPRINGGREEN", new ColorRecord(0, 250, 154));
+    colorDb.put("SEA GREEN", new ColorRecord(46, 139, 87));
+    colorDb.put("SEAGREEN", new ColorRecord(46, 139, 87));
+    colorDb.put("MEDIUM SEA GREEN", new ColorRecord(60, 179, 113));
+    colorDb.put("MEDIUMSEAGREEN", new ColorRecord(60, 179, 113));
+    colorDb.put("AQUAMARINE", new ColorRecord(112, 216, 144));
+    colorDb.put("LIGHTGREEN", new ColorRecord(144, 238, 144));
+    colorDb.put("PALE GREEN", new ColorRecord(152, 251, 152));
+    colorDb.put("PALEGREEN", new ColorRecord(152, 251, 152));
+    colorDb.put("MEDIUM AQUAMARINE", new ColorRecord(102, 205, 170));
+    colorDb.put("MEDIUMAQUAMARINE", new ColorRecord(102, 205, 170));
+    colorDb.put("TURQUOISE", new ColorRecord(64, 224, 208));
+    colorDb.put("LIGHTSEAGREEN", new ColorRecord(32, 178, 170));
+    colorDb.put("MEDIUM TURQUOISE", new ColorRecord(72, 209, 204));
+    colorDb.put("MEDIUMTURQUOISE", new ColorRecord(72, 209, 204));
+    colorDb.put("HONEYDEW", new ColorRecord(240, 255, 240));
+    colorDb.put("MINTCREAM", new ColorRecord(245, 255, 250));
+    colorDb.put("ROYALBLUE", new ColorRecord(65, 105, 225));
+    colorDb.put("DODGERBLUE", new ColorRecord(30, 144, 255));
+    colorDb.put("DEEPSKYBLUE", new ColorRecord(0, 191, 255));
+    colorDb.put("CORNFLOWERBLUE", new ColorRecord(100, 149, 237));
+    colorDb.put("STEEL BLUE", new ColorRecord(70, 130, 180));
+    colorDb.put("STEELBLUE", new ColorRecord(70, 130, 180));
+    colorDb.put("LIGHTSKYBLUE", new ColorRecord(135, 206, 250));
+    colorDb.put("DARK TURQUOISE", new ColorRecord(0, 206, 209));
+    colorDb.put("DARKTURQUOISE", new ColorRecord(0, 206, 209));
+    colorDb.put("CYAN", new ColorRecord(0, 255, 255));
+    colorDb.put("AQUA", new ColorRecord(0, 255, 255));
+    colorDb.put("DARKCYAN", new ColorRecord(0, 139, 139));
+    colorDb.put("TEAL", new ColorRecord(0, 128, 128));
+    colorDb.put("SKY BLUE", new ColorRecord(135, 206, 235));
+    colorDb.put("SKYBLUE", new ColorRecord(135, 206, 235));
+    colorDb.put("CADET BLUE", new ColorRecord(96, 160, 160));
+    colorDb.put("CADETBLUE", new ColorRecord(95, 158, 160));
+    colorDb.put("DARK SLATE GRAY", new ColorRecord(47, 79, 79));
+    colorDb.put("DARKSLATEGRAY", new ColorRecord(47, 79, 79));
+    colorDb.put("LIGHTSLATEGRAY", new ColorRecord(119, 136, 153));
+    colorDb.put("SLATEGRAY", new ColorRecord(112, 128, 144));
+    colorDb.put("LIGHT STEEL BLUE", new ColorRecord(176, 196, 222));
+    colorDb.put("LIGHTSTEELBLUE", new ColorRecord(176, 196, 222));
+    colorDb.put("LIGHT BLUE", new ColorRecord(173, 216, 230));
+    colorDb.put("LIGHTBLUE", new ColorRecord(173, 216, 230));
+    colorDb.put("POWDERBLUE", new ColorRecord(176, 224, 230));
+    colorDb.put("PALETURQUOISE", new ColorRecord(175, 238, 238));
+    colorDb.put("LIGHTCYAN", new ColorRecord(224, 255, 255));
+    colorDb.put("ALICEBLUE", new ColorRecord(240, 248, 255));
+    colorDb.put("AZURE", new ColorRecord(240, 255, 255));
+    colorDb.put("MEDIUM BLUE", new ColorRecord(0, 0, 205));
+    colorDb.put("MEDIUMBLUE", new ColorRecord(0, 0, 205));
+    colorDb.put("DARKBLUE", new ColorRecord(0, 0, 139));
+    colorDb.put("MIDNIGHT BLUE", new ColorRecord(25, 25, 112));
+    colorDb.put("MIDNIGHTBLUE", new ColorRecord(25, 25, 112));
+    colorDb.put("NAVY", new ColorRecord(36, 36, 140));
+    colorDb.put("BLUE", new ColorRecord(0, 0, 255));
+    colorDb.put("INDIGO", new ColorRecord(75, 0, 130));
+    colorDb.put("BLUE VIOLET", new ColorRecord(138, 43, 226));
+    colorDb.put("BLUEVIOLET", new ColorRecord(138, 43, 226));
+    colorDb.put("MEDIUM SLATE BLUE", new ColorRecord(123, 104, 238));
+    colorDb.put("MEDIUMSLATEBLUE", new ColorRecord(123, 104, 238));
+    colorDb.put("SLATE BLUE", new ColorRecord(106, 90, 205));
+    colorDb.put("SLATEBLUE", new ColorRecord(106, 90, 205));
+    colorDb.put("PURPLE", new ColorRecord(160, 32, 240));
+    colorDb.put("DARK SLATE BLUE", new ColorRecord(72, 61, 139));
+    colorDb.put("DARKSLATEBLUE", new ColorRecord(72, 61, 139));
+    colorDb.put("DARKVIOLET", new ColorRecord(148, 0, 211));
+    colorDb.put("DARK ORCHID", new ColorRecord(153, 50, 204));
+    colorDb.put("DARKORCHID", new ColorRecord(153, 50, 204));
+    colorDb.put("MEDIUMPURPLE", new ColorRecord(147, 112, 219));
+    colorDb.put("CORNFLOWER BLUE", new ColorRecord(68, 64, 108));
+    colorDb.put("MEDIUM ORCHID", new ColorRecord(186, 85, 211));
+    colorDb.put("MEDIUMORCHID", new ColorRecord(186, 85, 211));
+    colorDb.put("MAGENTA", new ColorRecord(255, 0, 255));
+    colorDb.put("FUCHSIA", new ColorRecord(255, 0, 255));
+    colorDb.put("DARKMAGENTA", new ColorRecord(139, 0, 139));
+    colorDb.put("VIOLET", new ColorRecord(238, 130, 238));
+    colorDb.put("PLUM", new ColorRecord(221, 160, 221));
+    colorDb.put("LAVENDER", new ColorRecord(230, 230, 250));
+    colorDb.put("THISTLE", new ColorRecord(216, 191, 216));
+    colorDb.put("GHOSTWHITE", new ColorRecord(248, 248, 255));
+    colorDb.put("WHITE", new ColorRecord(255, 255, 255));
+    colorDb.put("WHITESMOKE", new ColorRecord(245, 245, 245));
+    colorDb.put("GAINSBORO", new ColorRecord(220, 220, 220));
+    colorDb.put("LIGHT GRAY", new ColorRecord(211, 211, 211));
+    colorDb.put("LIGHTGRAY", new ColorRecord(211, 211, 211));
+    colorDb.put("SILVER", new ColorRecord(192, 192, 192));
+    colorDb.put("GRAY", new ColorRecord(190, 190, 190));
+    colorDb.put("DARK GRAY", new ColorRecord(169, 169, 169));
+    colorDb.put("DARKGRAY", new ColorRecord(169, 169, 169));
+    colorDb.put("DIM GRAY", new ColorRecord(105, 105, 105));
+    colorDb.put("DIMGRAY", new ColorRecord(105, 105, 105));
+    colorDb.put("BLACK", new ColorRecord(0, 0, 0));
+
+
+
+
+
+
+
 
 
 })();
