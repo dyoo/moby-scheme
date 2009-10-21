5c3599e70fda2a7eb3fddb34665c899b854a9ca0
Danny Yoo <dyoo@cs.wpi.edu>
2009-10-21 13:15:41 -0400

    planet archive making script.

--- make-planet-archive.sh
+++ make-planet-archive.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 OLDDIR=`pwd`
-git archive --format=tar --prefix=moby/ HEAD | (cd /var/tmp && tar xf -)
-cd /var/tmp/moby
-
+mkdir -p tmp
+rm -rf tmp/moby
+git archive --format=tar --prefix=moby/ HEAD | (cd tmp && tar xf -)
+cd tmp/moby
+rm -rf sandbox
+cd $OLDDIR/tmp
+planet create moby
 cd $OLDDIR
+cp tmp/moby.plt .
