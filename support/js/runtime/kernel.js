54469a308163b853c6705fefb1dbe948f9c29fac
Danny Yoo <dyoo@cs.wpi.edu>
2009-11-04 14:48:04 -0500

    At least IE isn't complaining, but canvases still aren't showing.

--- support/js/runtime/kernel.js
+++ support/js/runtime/kernel.js
@@ -13,6 +13,35 @@ var plt = plt || {};
 
 (function() {
 
+    // Compatibility hack: add Array.indexOf if it doesn't exist.
+    if(!Array.indexOf){
+	Array.prototype.indexOf = function(obj){
+	    for(var i=0; i<this.length; i++){
+		if(this[i]==obj){
+		    return i;
+		}
+	    }
+	    return -1;
+	}
+    }
+
+    // Ugly compability hack for IE: ensure that Node is defined.
+    if (!window['Node']) {
+	window.Node = new Object();
+	Node.ELEMENT_NODE = 1;
+	Node.ATTRIBUTE_NODE = 2;
+	Node.TEXT_NODE = 3;
+	Node.CDATA_SECTION_NODE = 4;
+	Node.ENTITY_REFERENCE_NODE = 5;
+	Node.ENTITY_NODE = 6;
+	Node.PROCESSING_INSTRUCTION_NODE = 7;
+	Node.COMMENT_NODE = 8;
+	Node.DOCUMENT_NODE = 9;
+	Node.DOCUMENT_TYPE_NODE = 10;
+	Node.DOCUMENT_FRAGMENT_NODE = 11;
+	Node.NOTATION_NODE = 12;
+    }
+
     
 
     // Compatibility for attaching events to nodes.
