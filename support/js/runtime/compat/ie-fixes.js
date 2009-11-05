d051c98aeff0bd21209cd9bda4e09d2d7a1d3786
Danny Yoo <dyoo@cs.wpi.edu>
2009-11-05 13:08:02 -0500

    Moved some of the ie specific stuff out to ie-fixes.js

---
+++ support/js/runtime/compat/ie-fixes.js
@@ -0,0 +1,17 @@
+// Ugly compability hack for IE: ensure that Node is defined.
+if (window && !window['Node']) {
+    window.Node = new Object();
+    Node.ELEMENT_NODE = 1;
+    Node.ATTRIBUTE_NODE = 2;
+    Node.TEXT_NODE = 3;
+    Node.CDATA_SECTION_NODE = 4;
+    Node.ENTITY_REFERENCE_NODE = 5;
+    Node.ENTITY_NODE = 6;
+    Node.PROCESSING_INSTRUCTION_NODE = 7;
+    Node.COMMENT_NODE = 8;
+    Node.DOCUMENT_NODE = 9;
+    Node.DOCUMENT_TYPE_NODE = 10;
+    Node.DOCUMENT_FRAGMENT_NODE = 11;
+    Node.NOTATION_NODE = 12;
+}
+    
--- support/js/runtime/kernel.js
+++ support/js/runtime/kernel.js
@@ -24,24 +24,6 @@ var plt = plt || {};
 	    return -1;
 	}
     }
-
-    // Ugly compability hack for IE: ensure that Node is defined.
-    if (!window['Node']) {
-	window.Node = new Object();
-	Node.ELEMENT_NODE = 1;
-	Node.ATTRIBUTE_NODE = 2;
-	Node.TEXT_NODE = 3;
-	Node.CDATA_SECTION_NODE = 4;
-	Node.ENTITY_REFERENCE_NODE = 5;
-	Node.ENTITY_NODE = 6;
-	Node.PROCESSING_INSTRUCTION_NODE = 7;
-	Node.COMMENT_NODE = 8;
-	Node.DOCUMENT_NODE = 9;
-	Node.DOCUMENT_TYPE_NODE = 10;
-	Node.DOCUMENT_FRAGMENT_NODE = 11;
-	Node.NOTATION_NODE = 12;
-    }
-
     
 
     // Compatibility for attaching events to nodes.
@@ -2149,7 +2131,7 @@ var plt = plt || {};
 	
 	// KLUDGE: IE compatibility uses /js/excanvas.js, and dynamic
 	// elements must be marked this way.
-	if (typeof window.G_vmlCanvasManager != 'undefined') {
+	if (window && typeof window.G_vmlCanvasManager != 'undefined') {
 	    canvas.style.display = 'none';
 	    document.body.appendChild(canvas);
 	    canvas = window.G_vmlCanvasManager.initElement(canvas);
