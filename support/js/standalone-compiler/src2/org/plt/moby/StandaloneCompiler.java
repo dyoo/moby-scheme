040fbb04ffadba3499c63c01261b655b58d239c1
Danny Yoo <dyoo@cs.wpi.edu>
2010-01-05 00:22:04 -0500

    got the standalone compiler jar running, it seems.

--- support/js/standalone-compiler/src2/org/plt/moby/StandaloneCompiler.java
+++ support/js/standalone-compiler/src2/org/plt/moby/StandaloneCompiler.java
@@ -1,7 +1,8 @@
 package org.plt.moby;
 
 import org.mozilla.javascript.*;
-
+import java.util.Set;
+import java.util.HashSet;
 
 public class StandaloneCompiler {
     private _StandaloneCompiler impl;
@@ -12,6 +13,13 @@ public class StandaloneCompiler {
 
     public CompilationResult compile(String src) {
 	Object[] results = this.impl.compile(src);
-	return null;
+	String compiledText = results[0].toString();
+	Scriptable permArray = (Scriptable) results[1];
+	double length = (Double) permArray.get("length", permArray);
+	Set<String> permissions = new HashSet<String>();
+	for (int i = 0; i < length; i++) {
+	    permissions.add(permArray.get(i, permArray).toString());
+	}
+	return new CompilationResult(compiledText, permissions);
     }
 }
--- support/js/standalone-compiler/src2/org/plt/moby/TestStandaloneCompiler.java
+++ support/js/standalone-compiler/src2/org/plt/moby/TestStandaloneCompiler.java
@@ -1,4 +1,16 @@
 package org.plt.moby;
 
+// FIXME: make these real unit tests.
 public class TestStandaloneCompiler {
+    public static void main(String[] args) {
+	StandaloneCompiler c = new StandaloneCompiler();
+	CompilationResult r = c.compile("(define (f x a b) (* x x)) (f 42 0 1)  (js-big-bang 0 (on-location-change f))");
+	System.out.println(r.getJs());
+	System.out.println(r.getPerms());
+
+
+	r = c.compile("(define (g y) (+ y 42)) (g 17)");
+	System.out.println(r.getJs());
+	System.out.println(r.getPerms());
+    }
 }
