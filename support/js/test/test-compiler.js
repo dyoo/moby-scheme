b166eee06c845ca658d4e97a027350ceed65c851
Danny Yoo <dyoo@cs.wpi.edu>
2010-01-28 01:15:45 -0500

    Added helpers in the test-compiler.js to pick out the type of the exception thrown.

--- support/js/test/test-compiler.js
+++ support/js/test/test-compiler.js
@@ -36,15 +36,135 @@ function init() {
     }
 
 
+    var errorStructModule = 
+	plt.Kernel.invokeModule("moby/runtime/error-struct");
+
+    var errorStructToDomModule = 
+	plt.Kernel.invokeModule("moby/runtime/error-struct-to-dom");
+    
+
+    // Returns true if the thing is a moby error
+    var isMobyError = function(x) {
+	return errorStructModule.EXPORTS.moby_dash_error_question_(x);
+    }
+    var mobyErrorType = function(x) {
+	return errorStructModule.EXPORTS.moby_dash_error_dash_error_dash_type(x);
+    }
+
+
+    //////////////////////////////////////////////////////////////////////
+
+
+    // errorType -> boolean
+    var isUnclosedLexicalToken = function(x) {
+	return errorStructModule.EXPORTS.moby_dash_error_dash_type_colon_unclosed_dash_lexical_dash_token_question_(x);
+    }
+
+    // errorType -> boolean
+    var isUnrecognizedLexicalToken = function(x) {
+	return errorStructModule.EXPORTS.moby_dash_error_dash_type_colon_unrecognized_dash_lexical_dash_token_question_(x);
+    }
+
+
+    // errorType -> boolean
+    var isUnsupportedLexicalToken = function(x) {
+	return errorStructModule.EXPORTS.moby_dash_error_dash_type_colon_unsupported_dash_lexical_dash_token_question_(x);
+    }
+
+
+    // errorType -> boolean
+    var isUnsupportedExpressionForm = function(x) {
+	return errorStructModule.EXPORTS.moby_dash_error_dash_type_colon_unsupported_dash_expression_dash_form_question_(x);
+    }
+
+    // errorType -> boolean
+    var isUnclosedParentheses = function(x) {
+	return errorStructModule.EXPORTS.moby_dash_error_dash_type_colon_unclosed_dash_parentheses_question_(x);
+    }
+
+    // errorType -> boolean
+    var isMissingExpression = function(x) {
+	return errorStructModule.EXPORTS.moby_dash_error_dash_type_colon_missing_dash_expression_question_(x);
+    }
+
+    // errorType -> boolean
+    var isDuplicateIdentifier = function(x) {
+	return errorStructModule.EXPORTS.moby_dash_error_dash_type_colon_duplicate_dash_identifier_question_(x);
+    }
+
+    // errorType -> boolean
+    var isExpectedIdentifier = function(x) {
+	return errorStructModule.EXPORTS.moby_dash_error_dash_type_colon_expected_dash_identifier_question_(x);
+    }
+
+    // errorType -> boolean
+    var isUndefinedIdentifier = function(x) {
+	return errorStructModule.EXPORTS.moby_dash_error_dash_type_colon_undefined_dash_identifier_question_(x);
+    }
+
+    // errorType -> boolean
+    var isStructureIdentifierNotExpression = function(x) {
+	return errorStructModule.EXPORTS.moby_dash_error_dash_type_colon_structure_dash_identifier_dash_not_dash_expression_question_(x);
+    }
+
+    // errorType -> boolean
+    var isProvidedNameNotDefined = function(x) {
+	return errorStructModule.EXPORTS.moby_dash_error_dash_type_colon_provided_dash_name_dash_not_dash_defined_question_(x);
+    }
+
+    // errorType -> boolean
+    var isProvidedStructureNotStructure = function(x) {
+	return errorStructModule.EXPORTS.moby_dash_error_dash_type_colon_provided_dash_structure_dash_not_dash_structure_question_(x);
+    }
+
+    // errorType -> boolean
+    var isUnknownModule = function(x) {
+	return errorStructModule.EXPORTS.moby_dash_error_dash_type_colon_unknown_dash_module_question_(x);
+    }
+
+
+
+    // errorType -> boolean
+    var isApplicationArity = function(x) {
+	return errorStructModule.EXPORTS.moby_dash_error_dash_type_colon_application_dash_arity_question_(x);
+    }
+
+
+    // errorType -> boolean
+    var isApplicationOperatorNotAFunction = function(x) {
+	return errorStructModule.EXPORTS.moby_dash_error_dash_type_colon_application_dash_operator_dash_not_dash_a_dash_function_question_(x);
+    }
+
+
+    // errorType -> boolean
+    var isTypeMismatch = function(x) {
+	return errorStructModule.EXPORTS.moby_dash_error_dash_type_colon_type_dash_mismatch_question_(x);
+    }
+
+
+    // errorType -> boolean
+    var isIndexOutOfBounds = function(x) {
+	return errorStructModule.EXPORTS.moby_dash_error_dash_type_colon_index_dash_out_dash_of_dash_bounds_question_(x);
+    }
+
+
+    // errorType -> boolean
+    var isConditionalExhausted = function(x) {
+	return errorStructModule.EXPORTS.moby_dash_error_dash_type_colon_conditional_dash_exhausted_question_(x);
+    }
+
+    
+
+
+    //////////////////////////////////////////////////////////////////////
+
+
+
     try {
 	run("(begin (+ 1 2) (- 3 4) (define j 5) (* 2 3))");
     } catch(e) {
-	var errorStructModule = 
-	    plt.Kernel.invokeModule("moby/runtime/error-struct");
-	var errorStructToDomModule = 
-	    plt.Kernel.invokeModule("moby/runtime/error-struct-to-dom");
-	console.log(e._fields[1]._constructorName);
-	console.log(errorStructToDomModule.EXPORTS.moby_dash_error_dash_struct_dash_to_dash_dom_dash_sexp(e));
+	console.log(isMobyError(e));
+	console.log(isUndefinedIdentifier(mobyErrorType(e)));
     }
 
 
@@ -55,7 +175,6 @@ function init() {
 	
         teardown: function() {},
 	
-
 
 	testSimpleDatum: function() {
 	    this.assert(isEqual(number(42), run("42")))
