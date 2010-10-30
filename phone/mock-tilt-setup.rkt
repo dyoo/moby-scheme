c0d715733bc41e8188aaf0ad246ed52942fa5611
Danny Yoo <dyoo@cs.wpi.edu>
2010-10-30 14:42:56 -0400

    whoops, forgot to commit mock-tilt-setup.rkt

---
+++ phone/mock-tilt-setup.rkt
@@ -0,0 +1,15 @@
+#lang planet dyoo/js-vm:1:4/js-impl
+
+(require-js "mock-tilt-setup.js")
+(require (planet dyoo/js-vm:1:4/ffi/ffi))
+
+
+;; mock-acceleration-setup: (world number number number -> world) -> js-shutdown-function
+(provide mock-acceleration-setup)
+
+
+;; mock-tilt-setup: (world number number number -> world) -> js-shutdown-function
+(provide mock-tilt-setup)
+
+;; mock-shake-setup: (world -> world) -> js-shutdown-function
+(provide mock-shake-setup)
