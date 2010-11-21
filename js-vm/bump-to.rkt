71cc98dc0877afefd95ea2251fe8fc27be5e29d3
Danny Yoo <dyoo@cs.wpi.edu>
2010-11-20 21:17:22 -0500

    writing the rest of the utility

--- js-vm/bump-to.rkt
+++ js-vm/bump-to.rkt
@@ -7,9 +7,21 @@
 		     (bytes=? (filename-extension p)
 			      #"rkt")))))
 
+(define (change-version b from to)
+  (regexp-replace* (format ":~a" from)
+		   b
+		   (format ":~a" to)))
+		   
+
+(define from (vector-ref (current-command-line-arguments) 0))
+(define to (vector-ref (current-command-line-arguments) 1))
+(printf "Changing ~a to ~a\n"
+	from to)
+(printf "Press enter to continue...")
+(read-line)
 (for ([f files-to-change])
   (let ([b (file->bytes f)])
     (call-with-output-file f
       (lambda (op)
-	(write-bytes b op))
-      #:exists replace)))
+	(write-bytes (change-version b) op))
+      #:exists 'replace)))
